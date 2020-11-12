fd8b634f1e48cd60d401704aaf556466a54720f3
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2020-11-11 22:27:32 -0300

    tenth commit agregando los ultimos detalles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 $(function(){
 
+    
     $("a").click(function(event){
 
         if(this.hash !== "") {
             event.preventDefault();
 
-            var gato = this.hash;
+            var bajar = this.hash;
 
             $("html, body").animate({
-                scrollTop: $(gato).offset().top
-            }, 1000, function(){
-                window.location.hash = gato;
+                scrollTop: $(bajar).offset().top
+            }, 900, function(){
+                window.location.hash = bajar;
             });
         }
     });
